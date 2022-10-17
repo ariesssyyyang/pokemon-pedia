88e79ee4eb1b7c2fd8df0c27d6f932fabb55d713
Aries Yang <ariesssyyyang@gmail.com>
2022-10-17 12:16:34 +0800

    Create a new project

---
+++ PokemonPedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonPedia
+//
+//  Created by Aries Yang on 2022/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
